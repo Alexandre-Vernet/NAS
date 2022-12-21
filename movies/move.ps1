55be411e9d8116bf52aa37a94bcdbc114e2f5791
Alex <alexandre.vernet@g-mail.fr>
2022-12-21 17:44:58 +0100

    feat: Create function Move-Files

--- movies/move.ps1
+++ movies/move.ps1
@@ -1,6 +1,10 @@
 # Connect to the remote server
 ssh Alex@192.168.1.142 -p 13
-echo Connected to the remote server
+Write-Output "Connected to the remote server"
 
-# Move all downloaded files to the folder "Films"
-find /volume1/Download/Transmission/complete/ -mindepth 2 -type f -print -exec mv {}  /volume1/home/Alex/Films  \;
+function Move-Files {
+    # Move all downloaded files to the folder "Films"
+    find /volume1/Download/Transmission/complete/ -mindepth 2 -type f -print -exec mv {}  /volume1/home/Alex/Films  \;
+}
+
+Move-Files
