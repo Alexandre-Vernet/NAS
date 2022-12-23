eaf81a1375ed8825765c8a5b50d89307ac2a94e8
Alex <alexandre.vernet@g-mail.fr>
2022-12-23 21:54:11 +0100

    feat: Add move-downloaded-movies.sh

---
+++ move-downloaded-movies.sh
@@ -0,0 +1,2 @@
+find /volume1/Download/Transmission/complete/ -mindepth 2 -type f -print -exec mv {}  /volume1/home/Alex/Films  \;
+find /volume1/Download/Transmission/complete/ -mindepth 1 -type f -print -exec mv {}  /volume1/home/Alex/Films  \;
