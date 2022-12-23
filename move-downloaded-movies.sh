244c8fcf1ae3ec2d27aef0ae6710f8102f726f17
Alex <alexandre.vernet@g-mail.fr>
2022-12-23 21:55:24 +0100

    feat: Add comments

--- move-downloaded-movies.sh
+++ move-downloaded-movies.sh
@@ -1,2 +1,3 @@
+# Move movies from the download directory including sub-folders to the movies directory
 find /volume1/Download/Transmission/complete/ -mindepth 2 -type f -print -exec mv {}  /volume1/home/Alex/Films  \;
 find /volume1/Download/Transmission/complete/ -mindepth 1 -type f -print -exec mv {}  /volume1/home/Alex/Films  \;
