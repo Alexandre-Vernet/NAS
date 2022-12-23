053c9955047502d02efd2298683c6c7d9ede95a5
Alex <alexandre.vernet@g-mail.fr>
2022-12-23 22:40:42 +0100

    feat: Move only file extension .mkv / .avi / .mp4

--- move-downloaded-movies.sh
+++ move-downloaded-movies.sh
@@ -1,3 +1,7 @@
-# Move movies from the download directory including sub-folders to the movies directory
-find /volume1/Download/Transmission/complete/ -mindepth 2 -type f -print -exec mv {}  /volume1/home/Alex/Films  \;
-find /volume1/Download/Transmission/complete/ -mindepth 1 -type f -print -exec mv {}  /volume1/home/Alex/Films  \;
+# Move all downloaded movies to /volume1/home/Alex/Films/
+
+# Move all downloaded movies from /Transmission/complete/
+find /volume1/Download/Transmission/complete/ -mindepth 1 -type f \( -iname "*.mkv" -o -name "*.avi" -o -iname "*mp4" \) -print -exec mv {}  /volume1/home/Alex/Films/  \;
+
+# Move all downloaded movies from /Transmission/complete/ including sub-folders
+find /volume1/Download/Transmission/complete/ -mindepth 2 -type f \( -iname "*.mkv" -o -name "*.avi" -o -iname "*mp4" \) -print -exec mv {}  /volume1/home/Alex/Films/  \;
