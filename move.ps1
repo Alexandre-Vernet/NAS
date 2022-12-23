94a6017fff4a0e847d06678fb7adf57c032197e9
Alex <alexandre.vernet@g-mail.fr>
2022-12-24 00:27:51 +0100

    clean: Add complete path to move-downloaded-movies.sh

--- move.ps1
+++ move.ps1
@@ -1,2 +1,2 @@
 # Connect to the server and move all downloaded movies to the movies folder
-ssh -t Alex@192.168.1.142 -p 13 'sh move-downloaded-movies.sh'
+ssh -t Alex@192.168.1.142 -p 13 'sh /volume1/home/Alex/move-downloaded-movies.sh'
