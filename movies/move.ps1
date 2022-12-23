ba106dd7d69b186442dac5207ecde01e4ae04b45
Alex <alexandre.vernet@g-mail.fr>
2022-12-23 10:25:03 +0100

    feat: Run shell instead command

--- movies/move.ps1
+++ movies/move.ps1
@@ -1,2 +1,2 @@
 # Connect to the server and move all downloaded movies to the movies folder
-ssh -t Alex@192.168.1.142 -p 13 'find /volume1/Download/Transmission/complete/ -mindepth 2 -type f -print -exec mv {}  /volume1/home/Alex/Films  \;'
+ssh -t Alex@192.168.1.142 -p 13 'sh move-downloaded-movies.sh'
